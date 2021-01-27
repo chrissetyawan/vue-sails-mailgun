f1864da82d9c1e7079bce21636942072a93c3d1a
chrissetyawan <chris_setyawan@yahoo.com>
2021-01-27 18:46:57 +0700

    add backend config files

---
+++ sails-backend/config/email.js
@@ -0,0 +1,8 @@
+module.exports.email = {
+  service: 'Mailgun',
+  auth: {
+    user: 'postmaster@sandboxxxxxxxxxxxxxxxxxxxx.mailgun.org',
+    pass: 'xxxxxxxxxxxxxxxxxxxxxxxxxx-xxxxxxx-xxxxxxxx'
+  },
+  testMode: false
+};
